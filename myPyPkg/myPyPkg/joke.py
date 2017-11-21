8af17d2bd509f6bb9327a69c07afa5a6ff179930
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-21 10:57:33 +0100

    and one and multi-line docstring

--- myPyPkg/myPyPkg/joke.py
+++ myPyPkg/myPyPkg/joke.py
@@ -1,4 +1,5 @@
 
 def joke():
+        """Print a joke"""
         return (u'Wenn ist das Nunst\00fcck git und Slotermeyer? Ja! ... '
                 u'Beiherhund das Oder die Flipperwaldt gersput.')
--- myPyPkg/myPyPkg/pythagorean.py
+++ myPyPkg/myPyPkg/pythagorean.py
@@ -1,4 +1,6 @@
 import math as m
 
 def pythagorean(x,y):
+        """Calculate calculates the length of the hypotenuse `c`, given `a` and `b`
+        the lengths of the triangle's other two sides, using the quation `a^2+b^2=c^2`"""
         return m.sqrt(x**2+y**2)
