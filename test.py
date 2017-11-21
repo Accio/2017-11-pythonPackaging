6d32c1ee3b29ba34a73e43928964751794b5fac6
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-21 10:57:43 +0100

    add testing cases for root, and help calling

--- test.py
+++ test.py
@@ -2,6 +2,17 @@
 
 import myPyPkg
 
+help(myPyPkg.joke)
 print(myPyPkg.joke())
 
+help(myPyPkg.pythagorean)
 print(myPyPkg.pythagorean(3,4))
+
+help(myPyPkg.root)
+print(myPyPkg.root(1,2,1))
+print(myPyPkg.root(1,-2,1))
+print(myPyPkg.root(1,0,-4))
+print(myPyPkg.root(1,0,4))
+
+## note that signPart is not exported
+## print(myPyPkg.signPart(1,0,-4))
