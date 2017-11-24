f1c1a73f5b9f7ad529086bb22b0bbab2fab6f5bb
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-24 15:24:48 +0100

    (1) add 'scripts' to register scripts (2) version bump

--- myPyPkg/setup.py
+++ myPyPkg/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='myPyPkg',
-      version='0.1.1',
+      version='0.1.2',
       description='A minimalistic python package',
       url='http://github.com/Accio/2017-11-pythonPackaging',
       author='Jitao David Zhang',
       author_email='davidvonpku@gmail.com',
       license='GPL-3',
       packages=['myPyPkg'],
+      scripts=['bin/joke.py'],
       zip_safe=False)
